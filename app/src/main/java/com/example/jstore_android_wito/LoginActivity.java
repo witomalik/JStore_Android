40e9d1a420507c63ae714e6a840db7dc6e940018
witomalik <witomalik@gmail.com>
2019-05-02 15:37:15 +0700

    Modul 9 TP

---
+++ app/src/main/java/com/example/jstore_android_wito/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.example.jstore_android_wito;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final EditText emailInput = (EditText) findViewById(R.id.emailInput);
+        final EditText passInput = (EditText) findViewById(R.id.passInput);
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = emailInput.getText().toString();
+                final String password = passInput.getText().toString();
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            if (jsonResponse != null){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login Success!").create().show();
+                            }
+                        }
+                        catch (JSONException e){
+                            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                            builder.setMessage("Login Failed!").create().show();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
+        final TextView registerClickable = (TextView) findViewById(R.id.registerClickable);
+        registerClickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regisIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(regisIntent);
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/jstore_android_wito/RegisterActivity.java
@@ -0,0 +1,66 @@
+package com.example.jstore_android_wito;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        final EditText nameInput = (EditText) findViewById(R.id.nameInput);
+        final EditText usernameInput = (EditText) findViewById(R.id.usernameInput);
+        final EditText emailInput = (EditText) findViewById(R.id.emailInput);
+        final EditText passInput = (EditText) findViewById(R.id.passInput);
+        final Button loginButton = (Button) findViewById(R.id.signupButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String name = nameInput.getText().toString();
+                final String username = usernameInput.getText().toString();
+                final String email = emailInput.getText().toString();
+                final String password = passInput.getText().toString();
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            if (jsonResponse != null){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Success!").create().show();
+                            }
+                        }
+                        catch (JSONException e){
+                            AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                            builder.setMessage("Register Failed!").create().show();
+                        }
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(name, username, email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
+        final TextView loginClickable = (TextView) findViewById(R.id.loginClickable);
+        loginClickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
